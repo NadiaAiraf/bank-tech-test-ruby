cb75bbb1f4f3242775b374f7ea1be051cf06fcb4
NadiaAiraf <aidanfaria@gmail.com>
2018-11-05 13:33:40 +0000

    some rubocop changes to the problem

--- spec/display_spec.rb
+++ spec/display_spec.rb
@@ -13,18 +13,21 @@ describe Display do
 
   describe '#join_line' do
     it 'returns the headers of a bank statement' do
-      expect(subject.join_line(Display::HEADINGS)).to eq 'date || credit || debit || balance'
+      answer = 'date || credit || debit || balance'
+      expect(subject.join_line(Display::HEADINGS)).to eq answer
     end
 
     it 'returns a line of date/credit/debit/balance' do
       transaction = ['10/01/12', nil, '500.00', '2500.00']
-      expect(subject.join_line(transaction)).to eq '10/01/12 ||  || 500.00 || 2500.00'
+      answer = '10/01/12 ||  || 500.00 || 2500.00'
+      expect(subject.join_line(transaction)).to eq answer
     end
   end
 
   describe '#print_statement' do
     it 'takes a users account and returns their statement' do
-      statement = "date || credit || debit || balance\n14/01/12 || 200.23 ||  || 500.52"
+      statement = "date || credit || debit || balance\n"\
+                  "14/01/12 || 200.23 ||  || 500.52"
       expect(subject.print_statement(mockAccount)).to eq statement
     end
   end
