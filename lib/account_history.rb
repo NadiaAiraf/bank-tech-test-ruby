63d7111bbbe81c57fe74cee5541cad4a88cd82f5
NadiaAiraf <aidanfaria@gmail.com>
2018-11-05 12:06:12 +0000

    fixed tests and methods in account history - 2 dp is what's entered now

--- lib/account_history.rb
+++ lib/account_history.rb
@@ -6,10 +6,10 @@ class AccountHistory
   end
 
   def credit_transaction(date, t_amount, t_balance)
-    @all_transactions.push(date: date, credit: t_amount, debit: nil, balance: t_balance)
+    @all_transactions.push(date: date, credit: '%.2f' % t_amount, debit: nil, balance: '%.2f' % t_balance)
   end
 
   def debit_transaction(date, t_amount, t_balance)
-    @all_transactions.push(date: date, credit: nil, debit: t_amount, balance: t_balance)
+    @all_transactions.push(date: date, credit: nil, debit: '%.2f' % t_amount, balance: '%.2f' % t_balance)
   end
 end
