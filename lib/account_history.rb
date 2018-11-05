1174e2fa59f70974f32e27ee655826c2a7ce21da
NadiaAiraf <aidanfaria@gmail.com>
2018-11-05 11:16:42 +0000

    added credit transaction to account history class, not sure it's the best way to go but will go on for now

--- lib/account_history.rb
+++ lib/account_history.rb
@@ -1,3 +1,14 @@
 class AccountHistory
+  attr_reader :account_history
+
+  def initialize
+    @account_history = []
+  end
+
+  def credit_transaction(date, t_amount, t_balance)
+    @account_history.push({ date: date,
+                            credit: t_amount,
+                            balance: t_balance })
+  end
 
 end
--- spec/account_history_spec.rb
+++ spec/account_history_spec.rb
@@ -1,5 +1,13 @@
 require 'account_history'
 
 describe AccountHistory do
-  
+  describe '#credit_transaction' do
+    it "adds a credit transaction to the account history array" do
+      subject.credit_transaction('14/01/12', 200, 500)
+      expect(subject.account_history).to eq [{ date: '14/01/12',
+                                              credit: 200,
+                                              balance: 500 }]
+    end
+  end
+
 end
