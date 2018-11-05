ee033150a80d8c2406648124a568226a65ef1010
NadiaAiraf <aidanfaria@gmail.com>
2018-11-05 14:31:10 +0000

    got rid of the annoying Time.now from my program because it's not exactly asked for

--- lib/account.rb
+++ lib/account.rb
@@ -8,12 +8,12 @@ class Account
     @account_history = account_history
   end
 
-  def deposit(amount, time = Time.now)
+  def deposit(amount, time)
     @current_balance += amount
     account_history.cr_transaction(time, amount, current_balance)
   end
 
-  def withdraw(amount, time = Time.now)
+  def withdraw(amount, time)
     raise 'Insufficient funds' if insufficient_funds(amount)
     @current_balance -= amount
     account_history.dr_transaction(time, amount, current_balance)
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -10,52 +10,52 @@ describe Account do
 
   describe '#deposit' do
     it 'adds £500 to the current balance' do
-      subject.deposit(500.00)
+      subject.deposit(500.00, '25/12/18')
       expect(subject.current_balance).to eq 500.00
     end
 
     it 'adds £1000 to the current balance' do
-      subject.deposit(1000.00)
+      subject.deposit(1000.00, '25/12/18')
       expect(subject.current_balance).to eq 1000.00
     end
 
     it 'adds £20.50 to the current balance' do
-      subject.deposit(20.50)
+      subject.deposit(20.50, '25/12/18')
       expect(subject.current_balance).to eq 20.50
     end
 
     it 'calls the method credit_transaction' do
-      subject.deposit(1000)
+      subject.deposit(1000, '25/12/18')
       expect(subject.account_history).to have_received(:cr_transaction)
     end
   end
 
   describe '#withdraw' do
     it 'withdraws 500 from the account' do
-      subject.deposit(5000.00)
-      subject.withdraw(500.00)
+      subject.deposit(5000.00, '25/12/18')
+      subject.withdraw(500.00, '25/12/18')
       expect(subject.current_balance).to eq 4500.00
     end
 
     it 'withdraws 75.69 from the account' do
-      subject.deposit(5000.00)
-      subject.withdraw(75.69)
+      subject.deposit(5000.00, '25/12/18')
+      subject.withdraw(75.69, '25/12/18')
       expect(subject.current_balance).to eq 4924.31
     end
 
     it 'errors when no money is in the account' do
-      expect { subject.withdraw(100) }.to raise_error 'Insufficient funds'
+      expect { subject.withdraw(100, '25/12/18') }.to raise_error 'Insufficient funds'
     end
 
     it 'calls the method dr_transaction' do
-      subject.deposit(1000)
-      subject.withdraw(500)
+      subject.deposit(1000, '25/12/18')
+      subject.withdraw(500, '25/12/18')
       expect(subject.account_history).to have_received(:dr_transaction)
     end
 
     it 'errors when money in account is not sufficient is in the account' do
-      subject.deposit(100)
-      expect { subject.withdraw 101 }.to raise_error 'Insufficient funds'
+      subject.deposit(100, '25/12/18')
+      expect { subject.withdraw(101, '25/12/18') }.to raise_error 'Insufficient funds'
     end
   end
 end
