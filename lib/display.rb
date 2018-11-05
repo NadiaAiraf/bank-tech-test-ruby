f6eb3bee92e9691b9c2e26a4b0c5758851fa043d
NadiaAiraf <aidanfaria@gmail.com>
2018-11-05 13:53:51 +0000

    changed Display to class methods, reversed order of account history array

--- lib/display.rb
+++ lib/display.rb
@@ -1,17 +1,19 @@
 class Display
   HEADINGS = %w[date credit debit balance].freeze
 
-  def print_statement(account)
+  def self.print_statement(account)
     statement_text = join_line(HEADINGS)
     account_array = account.account_history.all_transactions
-    statement_text + "\n" + account_history_line(account_array).join("\n")
+    statement_text += "\n" + account_history_line(account_array).join("\n")
+    puts statement_text
+    statement_text
   end
 
-  def account_history_line(array)
-    array.map { |element| join_line(element.values) }
+  def self.account_history_line(array)
+    array.map { |element| join_line(element.values) }.reverse
   end
 
-  def join_line(array)
+  def self.join_line(array)
     array.join(' || ')
   end
 end
--- spec/display_spec.rb
+++ spec/display_spec.rb
@@ -14,13 +14,13 @@ describe Display do
   describe '#join_line' do
     it 'returns the headers of a bank statement' do
       answer = 'date || credit || debit || balance'
-      expect(subject.join_line(Display::HEADINGS)).to eq answer
+      expect(Display.join_line(Display::HEADINGS)).to eq answer
     end
 
     it 'returns a line of date/credit/debit/balance' do
       transaction = ['10/01/12', nil, '500.00', '2500.00']
       answer = '10/01/12 ||  || 500.00 || 2500.00'
-      expect(subject.join_line(transaction)).to eq answer
+      expect(Display.join_line(transaction)).to eq answer
     end
   end
 
@@ -28,19 +28,19 @@ describe Display do
     it 'takes a users account and returns their statement' do
       statement = "date || credit || debit || balance\n"\
                   '14/01/12 || 200.23 ||  || 500.52'
-      expect(subject.print_statement(mockAccount)).to eq statement
+      expect(Display.print_statement(mockAccount)).to eq statement
     end
   end
 
   describe '#account_history_line' do
-    it 'takes an array of hashes and returns array of arrays' do
+    it 'takes an array of hashes and returns array of arrays in reverse' do
       hash_array = [{ date: '1/1/1', cr: '200.00', dr: nil, bal: '2500.00' },
                     { date: '2/2/2', cr: '300.00', dr: nil, bal: '2800.00' },
                     { date: '3/3/3', cr: '100.00', dr: nil, bal: '2900.00' }]
-      answer_array = ['1/1/1 || 200.00 ||  || 2500.00',
+      answer_array = ['3/3/3 || 100.00 ||  || 2900.00',
                       '2/2/2 || 300.00 ||  || 2800.00',
-                      '3/3/3 || 100.00 ||  || 2900.00']
-      expect(subject.account_history_line(hash_array)).to eq answer_array
+                      '1/1/1 || 200.00 ||  || 2500.00']
+      expect(Display.account_history_line(hash_array)).to eq answer_array
     end
   end
 end
