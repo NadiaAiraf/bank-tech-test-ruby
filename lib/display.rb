9b3e0636a6e2f73b3f0dcd0e31aaad70fb6ef1ec
NadiaAiraf <aidanfaria@gmail.com>
2018-11-05 13:12:46 +0000

    big commit: print_statement test, output is as expected, created new method to do things that I need to test

--- lib/display.rb
+++ lib/display.rb
@@ -1,8 +1,17 @@
 class Display
   HEADINGS = %w[date credit debit balance].freeze
 
-  def print_statement(_account)
-    puts 'hello'
+  def print_statement(account)
+    statement_text = join_line(HEADINGS)
+    statement_text += "\n" + account_history_line(account).join("\n")
+    return statement_text
+  end
+
+  def account_history_line(array)
+    new_array = array.account_history.all_transactions.map do |element|
+      element.map{ |k, v| v }.join(' || ')
+    end
+    return new_array
   end
 
   def join_line(array)
