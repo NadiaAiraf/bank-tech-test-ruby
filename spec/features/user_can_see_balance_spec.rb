bfe8e495e2f9ff974af2d81db2b89f9a2d1295cc
NadiaAiraf <aidanfaria@gmail.com>
2018-11-06 10:52:29 +0000

    feature test: depositing and withdrawing and printing

--- spec/features/user_can_see_balance_spec.rb
+++ spec/features/user_can_see_balance_spec.rb
@@ -9,4 +9,14 @@ RSpec.describe 'User interface' do
     account.deposit(450.73,'01/01/18')
     expect{ Display.print_statement(account) }.to output(answer).to_stdout
   end
+
+  it 'lets a user deposit and withdraw then shows those on the statement' do
+    answer = "date || credit || debit || balance\n"\
+             "02/01/18 ||  || 385.25 || 65.48\n"\
+             "01/01/18 || 450.73 ||  || 450.73\n"
+    account = Account.new
+    account.deposit(450.73,'01/01/18')
+    account.withdraw(385.25,'02/01/18')
+    expect{ Display.print_statement(account) }.to output(answer).to_stdout
+  end
 end
